67f176c9ccea28e60dc5622a7df490644bdc08c9
Harsha120903 <22211a6762@bvrit.ac.in>
2025-11-01 23:48:00 +0530

    changed url

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://linkedin-clone-frontend-otwq.onrender.com', credentials: true }));
 app.use(express.json());
 
 // Routes
