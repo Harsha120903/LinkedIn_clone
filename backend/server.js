e26f536d99cb66c6f6c903491a68866d1e267984
Harsha120903 <22211a6762@bvrit.ac.in>
2025-11-02 12:06:45 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
